07a6dd9b4c7ecf606651d845b1a9ff0c8038eb05
qingling.shen <qingling.shen@newtouch.cn>
2016-04-13 00:57:09 +0800

    webpack dev server

--- frontend/entry.js
+++ frontend/entry.js
@@ -1,1 +1,17 @@
-console.log('hello');
+import 'bootstrap-webpack';
+import React from 'react';
+import ReactDom from 'react-dom';
+
+console.log('hello, world');
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+ReactDom.render(<App />, document.body);
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -7,16 +7,44 @@ module.exports = {
     filename: "bundle.js"
   },
   module: {
-    loaders: [{
+    loaders: [{ //基本css
       test: /\.css$/,
       loader: "style!css"
-    }, {
+    }, { //babel-loader
       test: /\.jsx?$/,
       exclude: /(node_modules|bower_components)/, //这两种文件不编译
       loader: 'babel', // 'babel-loader' is also a legal name to reference
       query: {
         presets: ['react', 'es2015']
       }
+    }, { //bootstrap-webpack
+      test: /\.(woff|woff2)$/,
+      loader: "url-loader?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.ttf$/,
+      loader: "file-loader"
+    }, {
+      test: /\.eot$/,
+      loader: "file-loader"
+    }, {
+      test: /\.svg$/,
+      loader: "file-loader"
+    }, { //确保加载完jquery在加载bootstrap
+      test: /\/bootstrap\/js\//,
+      loader: 'imports?jQuery=jquery'
     }]
+  },
+  devServer: {
+    contentBase: __dirname, // 当前服务目录
+    port: 3000,
+    inline: true, //
+    historyApiFallback: true,
+    status: {
+      colors: true
+    },
+    // hot: true,
+    // proxy: {
+    //   '*': 'http://127.0,0.1:3001'
+    // }
   }
 };
