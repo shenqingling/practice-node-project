c327f52dfa03b8936d8f0025182d45b9cd02e6d9
qingling.shen <qingling.shen@newtouch.cn>
2016-04-25 23:26:37 +0800

    抽象出 TopicEditor

---
+++ frontend/component/TopicEditor.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import jQuery from 'jquery';
+import {addTopic} from '../lib/client';
+import {redirectURL} from '../lib/utils';
+
+export default class TopicEditor extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {};
+    if(props.topic){
+      this.setState(props.topic);
+    }
+  }
+
+  //
+  handleChange(name, e){
+    this.state[name] = e.target.value;
+  }
+
+  // 点击‘保存’
+  handleSubmit(e){
+    const $btn = jQuery(e.target);
+    $btn.button('loading');
+    this.props.onSave(this.state,() => {
+      $btn.button('reset');
+    });
+  }
+
+  render(){
+
+    return(
+      <div className="panel panel-primary">
+        <div className="panel-heading">{this.props.title}</div>
+        <div className="panel-body">
+          <form>
+            <div className="form-group">
+              <label htmlFor="ipt-title">标题</label>
+              <input type="text" className="form-control" id="ipt-title" onChange={this.handleChange.bind(this, 'title')} placeholder="" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="ipt-tags">标签</label>
+              <input type="text" className="form-control" id="ipt-tags" onChange={this.handleChange.bind(this, 'tags')} placeholder="" />
+              <p className="help-block">多个标签使用半角都好分隔</p>
+            </div>
+            <div className="form-group">
+              <label htmlFor="ipt-content">内容</label>
+              <textarea type="text" className="form-control" rows="10" id="ipt-content" onChange={this.handleChange.bind(this, 'content')} placeholder=""></textarea>
+            </div>
+            <button type="button" className="btn btn-primary" onClick={this.handleSubmit.bind(this)}>保存</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
