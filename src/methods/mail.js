e37b3b7b8021812ff3458df61a17071a492ce907
qingling.shen <qingling.shen@newtouch.cn>
2016-05-24 23:40:30 +0800

    完成发送邮件模板功能

--- src/methods/mail.js
+++ src/methods/mail.js
@@ -6,12 +6,23 @@
 * @author sql370 <sql370@qq.com>
 */
 
+import fs from 'fs';
+import path from 'path';
+import rd from 'rd';
+import ejs from 'ejs';
 import nodemailer from 'nodemailer';
 
 module.exports = function (done) {
 
   $.smtp = nodemailer.createTransport($.config.get('smtp'), {
     from: $.config.get('smtp.auth.user')
+  });
+
+  const templates = {};
+  rd.eachFileFilterSync(path.resolve(__dirname, '../../email_templates'), /\.html/, (f, s) => {
+    const name = path.basename(f, '.html');
+    const html = fs.readFileSync(f).toString();
+    templates[name] = ejs.compile(html);
   });
 
   $.method('mail.send').check({
@@ -25,10 +36,33 @@ module.exports = function (done) {
 
   });
 
-  // $.method('mail.send').call({
+  $.method('mail.sendTemplate').check({
+    to: {required: true}, // 多个邮箱地址以逗号分割
+    subject: {required: true},
+    template: {required: true}
+  });
+  $.method('mail.sendTemplate').register(function(params, callback){
+
+    const fn = templates[params.template];
+    if(!fn) throw new Error(`invaild email template "${params.template}"`);
+
+    const html = fn(params.data || {});
+
+    return $.method('mail.send').call({
+      to: params.to,
+      subject: params.subject,
+      html: html
+    });
+
+  });
+
+  // $.method('mail.sendTemplate').call({
   //   to: 'me@ucdok.com',
-  //   subject: '我的第一封邮件',
-  //   html: '<h1>啊哈哈哈,这是标题</h1><p>内容</p>'
+  //   subject: '欢迎',
+  //   template: 'welcome',
+  //   data: {
+  //     username: 'ucdoj'
+  //   }
   // },console.log);
 
   done();
